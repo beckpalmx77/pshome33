6add23bdf93b9776965127a6d2897445f6bfda4a
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-01-14 18:08:13 +0700

    up

---
+++ manage_house.php
@@ -0,0 +1,387 @@
+<?php
+include('includes/Header.php');
+if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['house_number']) == "") {
+    header("Location: index.php");
+} else {
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="th">
+    <body id="page-top">
+    <div id="wrapper">
+        <?php
+        include('includes/Side-Bar.php');
+        ?>
+
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php
+                include('includes/Top-Bar.php');
+                ?>
+                <!-- Container Fluid-->
+                <div class="container-fluid" id="container-wrapper">
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800"><?php echo urldecode($_GET['s']) ?></h1>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?php echo $_SESSION['dashboard_page'] ?>">Home</a>
+                            </li>
+                            <li class="breadcrumb-item"><?php echo urldecode($_GET['m']) ?></li>
+                            <li class="breadcrumb-item active"
+                                aria-current="page"><?php echo urldecode($_GET['s']) ?></li>
+                        </ol>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card mb-12">
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                </div>
+                                <div class="card-body">
+                                    <section class="container-fluid">
+
+                                        <div class="col-md-12 col-md-offset-2">
+                                            <label for="name_t"
+                                                   class="control-label"><b>เพิ่ม <?php echo urldecode($_GET['s']) ?></b></label>
+
+                                            <button type='button' name='btnAdd' id='btnAdd'
+                                                    class='btn btn-primary btn-xs'>Add
+                                                <i class="fa fa-plus"></i>
+                                            </button>
+                                        </div>
+
+                                        <div class="col-md-12 col-md-offset-2">
+                                            <table id='TableRecordList' class='display dataTable'>
+                                                <thead>
+                                                <tr>
+                                                    <th>บ้านเลขที่</th>
+                                                    <th>ซอย</th>
+                                                    <th>ชื่อผู้ติดต่อ</th>
+                                                    <th>หมายเลขโทรศัพท์</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                                </thead>
+                                                <tfoot>
+                                                <tr>
+                                                    <th>บ้านเลขที่</th>
+                                                    <th>ซอย</th>
+                                                    <th>ชื่อผู้ติดต่อ</th>
+                                                    <th>หมายเลขโทรศัพท์</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                                </tfoot>
+                                            </table>
+
+                                            <div id="result"></div>
+
+                                        </div>
+
+                                        <div class="modal fade" id="recordModal">
+                                            <div class="modal-dialog modal-lg">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h4 class="modal-title">Modal title</h4>
+                                                        <button type="button" class="close" data-dismiss="modal"
+                                                                aria-hidden="true">×
+                                                        </button>
+                                                    </div>
+                                                    <form method="post" id="recordForm">
+                                                        <div class="modal-body">
+                                                            <div class="modal-body">
+
+                                                                <div class="form-group">
+                                                                    <label for="house_number" class="control-label">บ้านเลขที่</label>
+                                                                    <input type="house_number" class="form-control"
+                                                                           id="house_number" name="house_number"
+                                                                           placeholder="">
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label for="contact_name"
+                                                                           class="control-label">ชื่อผู้ติดต่อ</label>
+                                                                    <input type="text" class="form-control"
+                                                                           id="contact_name"
+                                                                           name="contact_name"
+                                                                           required="required"
+                                                                           placeholder="">
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label for="alley"
+                                                                           class="control-label">ซอย</label>
+                                                                    <input type="text" class="form-control"
+                                                                           id="alley"
+                                                                           name="alley"
+                                                                           required="required"
+                                                                           placeholder="">
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label for="phone_number"
+                                                                           class="control-label">หมายเลขโทรศัพท์</label>
+                                                                    <input type="text" class="form-control"
+                                                                           id="phone_number"
+                                                                           name="phone_number"
+                                                                           required="required"
+                                                                           placeholder="">
+                                                                </div>
+
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <input type="hidden" name="id" id="id"/>
+                                                            <input type="hidden" name="action" id="action" value=""/>
+                                                            <span class="icon-input-btn">
+                                                                <i class="fa fa-check"></i>
+                                                            <input type="submit" name="save" id="save"
+                                                                   class="btn btn-primary" value="Save"/>
+                                                            </span>
+                                                            <button type="button" class="btn btn-danger"
+                                                                    data-dismiss="modal">Close <i
+                                                                        class="fa fa-window-close"></i>
+                                                            </button>
+                                                        </div>
+                                                    </form>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include('includes/Modal-Logout.php');
+    include('includes/Footer.php');
+    ?>
+
+
+    <!-- Scroll to top -->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/myadmin.min.js"></script>
+
+    <!-- Page level plugins -->
+
+    <script src="vendor/datatables/v11/bootbox.min.js"></script>
+    <script src="vendor/datatables/v11/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="vendor/datatables/v11/jquery.dataTables.min.css"/>
+    <link rel="stylesheet" href="vendor/datatables/v11/buttons.dataTables.min.css"/>
+
+    <style>
+
+        .icon-input-btn {
+            display: inline-block;
+            position: relative;
+        }
+
+        .icon-input-btn input[type="submit"] {
+            padding-left: 2em;
+        }
+
+        .icon-input-btn .fa {
+            display: inline-block;
+            position: absolute;
+            left: 0.65em;
+            top: 30%;
+        }
+    </style>
+    <script>
+        $(document).ready(function () {
+            $(".icon-input-btn").each(function () {
+                let btnFont = $(this).find(".btn").css("font-size");
+                let btnColor = $(this).find(".btn").css("color");
+                $(this).find(".fa").css({'font-size': btnFont, 'color': btnColor});
+            });
+        });
+    </script>
+
+    <script>
+
+        $("#contact_name").blur(function () {
+            let method = $('#action').val();
+            if (method === "ADD") {
+                let house_number = $('#house_number').val();
+                let contact_name = $('#contact_name').val();
+                let formData = {action: "SEARCH", house_number: house_number, contact_name: contact_name};
+                $.ajax({
+                    url: 'model/manage_house_process.php',
+                    method: "POST",
+                    data: formData,
+                    success: function (data) {
+                        if (data == 2) {
+                            alert("Duplicate มีข้อมูลนี้แล้วในระบบ กรุณาตรวจสอบ");
+                        }
+                    }
+                })
+            }
+        });
+
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            let formData = {action: "GET_HOUSE", sub_action: "GET_MASTER"};
+            let dataRecords = $('#TableRecordList').DataTable({
+                'lengthMenu': [[5, 10, 20, 50, 100], [5, 10, 20, 50, 100]],
+                'language': {
+                    search: 'ค้นหา', lengthMenu: 'แสดง _MENU_ รายการ',
+                    info: 'หน้าที่ _PAGE_ จาก _PAGES_',
+                    infoEmpty: 'ไม่มีข้อมูล',
+                    zeroRecords: "ไม่มีข้อมูลตามเงื่อนไข",
+                    infoFiltered: '(กรองข้อมูลจากทั้งหมด _MAX_ รายการ)',
+                    paginate: {
+                        previous: 'ก่อนหน้า',
+                        last: 'สุดท้าย',
+                        next: 'ต่อไป'
+                    }
+                },
+                'processing': true,
+                'serverSide': true,
+                'serverMethod': 'post',
+                'ajax': {
+                    'url': 'model/manage_house_process.php',
+                    'data': formData
+                },
+                'columns': [
+                    {data: 'house_number'},
+                    {data: 'alley'},
+                    {data: 'contact_name'},
+                    {data: 'phone_number'},
+                    {data: 'update'}
+                ]
+            });
+
+            <!-- *** FOR SUBMIT FORM *** -->
+            $("#recordModal").on('submit', '#recordForm', function (event) {
+                event.preventDefault();
+                $('#save').attr('disabled', 'disabled');
+                let formData = $(this).serialize();
+                //alert(formData);
+                $.ajax({
+                    url: 'model/manage_house_process.php',
+                    method: "POST",
+                    data: formData,
+                    success: function (data) {
+                        alertify.success(data);
+                        $('#recordForm')[0].reset();
+                        $('#recordModal').modal('hide');
+                        $('#save').attr('disabled', false);
+                        dataRecords.ajax.reload();
+                    }
+                })
+            });
+            <!-- *** FOR SUBMIT FORM *** -->
+
+
+        });
+
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            $("#btnAdd").click(function () {
+                $('#recordModal').modal('show');
+                $('#id').val("");
+                $('#house_number').val("");
+                $('#house_number').val("");
+                $('.modal-title').html("<i class='fa fa-plus'></i> ADD Record");
+                $('#action').val('ADD');
+                $('#save').val('Save');
+            });
+        });
+    </script>
+
+    <script>
+
+        $("#TableRecordList").on('click', '.update', function () {
+            let id = $(this).attr("id");
+            //alert(id);
+            let formData = {action: "GET_DATA", id: id};
+            $.ajax({
+                type: "POST",
+                url: 'model/manage_house_process.php',
+                dataType: "json",
+                data: formData,
+                success: function (response) {
+                    let len = response.length;
+                    for (let i = 0; i < len; i++) {
+                        let id = response[i].id;
+                        let house_number = response[i].house_number;
+                        let contact_name = response[i].contact_name;
+                        let phone_number = response[i].phone_number;
+                        let alley = response[i].alley;
+
+                        $('#recordModal').modal('show');
+                        $('#id').val(id);
+                        $('#house_number').val(house_number);
+                        $('#contact_name').val(contact_name);
+                        $('#phone_number').val(phone_number);
+                        $('#alley').val(alley);
+                        $('.modal-title').html("<i class='fa fa-plus'></i> Edit Record");
+                        $('#action').val('UPDATE');
+                        $('#save').val('Save');
+                    }
+                },
+                error: function (response) {
+                    alertify.error("error : " + response);
+                }
+            });
+        });
+
+    </script>
+
+    <script>
+
+        $("#TableRecordList").on('click', '.delete', function () {
+            let id = $(this).attr("id");
+            let formData = {action: "GET_DATA", id: id};
+            $.ajax({
+                type: "POST",
+                url: 'model/manage_house_process.php',
+                dataType: "json",
+                data: formData,
+                success: function (response) {
+                    let len = response.length;
+                    for (let i = 0; i < len; i++) {
+                        let id = response[i].id;
+                        let house_number = response[i].house_number;
+                        let contact_name = response[i].contact_name;
+                        let phone_number = response[i].phone_number;
+                        let alley = response[i].alley;
+
+                        $('#recordModal').modal('show');
+                        $('#id').val(id);
+                        $('#house_number').val(house_number);
+                        $('#contact_name').val(contact_name);
+                        $('#phone_number').val(phone_number);
+                        $('#alley').val(alley);
+                        $('.modal-title').html("<i class='fa fa-minus'></i> Delete Record");
+                        $('#action').val('DELETE');
+                        $('#save').val('Confirm Delete');
+                    }
+                },
+                error: function (response) {
+                    alertify.error("error : " + response);
+                }
+            });
+        });
+
+    </script>
+
+    </body>
+    </html>
+
+<?php } ?>
---
+++ model/manage_house_process.php
@@ -0,0 +1,229 @@
+<?php
+session_start();
+error_reporting(0);
+
+include('../config/connect_db.php');
+include('../config/lang.php');
+include('../util/record_util.php');
+
+
+if ($_POST["action"] === 'GET_DATA') {
+
+    $id = $_POST["id"];
+
+    $return_arr = array();
+
+    $sql_get = "SELECT * FROM ims_house "
+        . " WHERE ims_house.id = " . $id;
+
+    //$myfile = fopen("myqeury_1.txt", "w") or die("Unable to open file!");
+    //fwrite($myfile, $sql_get);
+    //fclose($myfile);
+
+    $statement = $conn->query($sql_get);
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($results as $result) {
+        $return_arr[] = array("id" => $result['id'],
+            "house_number" => $result['house_number'],
+            "contact_name" => $result['contact_name'],
+            "house_name" => $result['house_name'],
+            "phone_number" => $result['phone_number'],
+            "remark" => $result['remark'],
+            "alley" => $result['alley']);
+    }
+
+    echo json_encode($return_arr);
+
+}
+
+if ($_POST["action"] === 'SEARCH') {
+
+    if ($_POST["house_number"] !== '') {
+
+        $house_number = $_POST["house_number"];
+        $sql_find = "SELECT * FROM ims_house WHERE house_number = '" . $house_number . "'";
+        $nRows = $conn->query($sql_find)->fetchColumn();
+        if ($nRows > 0) {
+            echo 2;
+        } else {
+            echo 1;
+        }
+    }
+}
+
+if ($_POST["action"] === 'ADD') {
+    if ($_POST["contact_name"] !== '') {
+        $house_number = $_POST["house_number"];
+        $contact_name = $_POST["contact_name"];
+        $phone_number = $_POST["phone_number"];
+        $alley = $_POST["alley"];
+        $sql_find = "SELECT * FROM ims_house WHERE house_number = '" . $house_number . "'";
+        $nRows = $conn->query($sql_find)->fetchColumn();
+        if ($nRows > 0) {
+            echo $dup;
+        } else {
+            $sql = "INSERT INTO ims_house(house_number,contact_name,alley) 
+                    VALUES (:house_number,:contact_name,:alley)";
+            $query = $conn->prepare($sql);
+            $query->bindParam(':house_number', $house_number, PDO::PARAM_STR);
+            $query->bindParam(':contact_name', $contact_name, PDO::PARAM_STR);
+            $query->bindParam(':phone_number', $phone_number, PDO::PARAM_STR);
+            $query->bindParam(':alley', $alley, PDO::PARAM_STR);
+            $query->execute();
+            $lastInsertId = $conn->lastInsertId();
+
+            if ($lastInsertId) {
+                echo $save_success;
+            } else {
+                echo $error;
+            }
+        }
+    }
+}
+
+if ($_POST["action"] === 'UPDATE') {
+
+    if ($_POST["contact_name"] != '') {
+
+        $id = $_POST["id"];
+        $house_number = $_POST["house_number"];
+        $contact_name = $_POST["contact_name"];
+        $alley = $_POST["alley"];
+        $sql_find = "SELECT * FROM ims_house WHERE id = '" . $id . "'";
+        $nRows = $conn->query($sql_find)->fetchColumn();
+        if ($nRows > 0) {
+            $sql_update = "UPDATE ims_house SET house_number=:house_number,contact_name=:contact_name,alley=:alley            
+            WHERE id = :id";
+            $query = $conn->prepare($sql_update);
+            $query->bindParam(':house_number', $house_number, PDO::PARAM_STR);
+            $query->bindParam(':contact_name', $contact_name, PDO::PARAM_STR);
+            $query->bindParam(':alley', $alley, PDO::PARAM_STR);
+            $query->bindParam(':id', $id, PDO::PARAM_STR);
+            $query->execute();
+            echo $save_success;
+        }
+
+    }
+}
+
+if ($_POST["action"] === 'DELETE') {
+
+    $id = $_POST["id"];
+
+    $sql_find = "SELECT * FROM ims_house WHERE id = " . $id;
+    $nRows = $conn->query($sql_find)->fetchColumn();
+    if ($nRows > 0) {
+        try {
+            $sql = "DELETE FROM ims_house WHERE id = " . $id;
+            $query = $conn->prepare($sql);
+            $query->execute();
+            echo $del_success;
+        } catch (Exception $e) {
+            echo 'Message: ' . $e->getMessage();
+        }
+    }
+}
+
+if ($_POST["action"] === 'GET_HOUSE') {
+
+    ## Read value
+    $draw = $_POST['draw'];
+    $row = $_POST['start'];
+    $rowperpage = $_POST['length']; // Rows display per page
+    $columnIndex = $_POST['order'][0]['column']; // Column index
+    $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+    $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+    $searchValue = $_POST['search']['value']; // Search value
+    /*
+        $txt = "DDD " . $columnSortOrder;
+        $my_file = fopen("device_a.txt", "w") or die("Unable to open file!");
+        fwrite($my_file, $txt);
+        fclose($my_file);
+    */
+
+
+    $searchArray = array();
+
+## Search
+    $searchQuery = " ";
+    if ($searchValue != '') {
+        $searchQuery = " AND (house_number LIKE :house_number or
+        contact_name LIKE :contact_name ) ";
+        $searchArray = array(
+            'house_number' => "%$searchValue%",
+            'contact_name' => "%$searchValue%",
+        );
+    }
+
+## Total number of records without filtering
+    $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM ims_house ");
+    $stmt->execute();
+    $records = $stmt->fetch();
+    $totalRecords = $records['allcount'];
+
+## Total number of records with filtering
+    $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM ims_house WHERE 1 " . $searchQuery);
+    $stmt->execute($searchArray);
+    $records = $stmt->fetch();
+    $totalRecordwithFilter = $records['allcount'];
+
+## Fetch records
+
+    $stmt = $conn->prepare("SELECT * FROM v_ims_house WHERE 1 " . $searchQuery . " LIMIT :limit,:offset");
+
+    /*
+        $txt = $searchQuery . " | " . $columnName . " | " . $columnSortOrder;
+        $my_file = fopen("device_b.txt", "w") or die("Unable to open file!");
+        fwrite($my_file, $txt);
+        fclose($my_file);
+    */
+
+
+// Bind values
+    foreach ($searchArray as $key => $search) {
+        $stmt->bindValue(':' . $key, $search, PDO::PARAM_STR);
+    }
+
+    $stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
+    $stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
+    $stmt->execute();
+    $empRecords = $stmt->fetchAll();
+    $data = array();
+
+    foreach ($empRecords as $row) {
+
+        if ($_POST['sub_action'] === "GET_MASTER") {
+            $data[] = array(
+                "id" => $row['id'],
+                "house_number" => $row['house_number'],
+                "alley" => $row['alley'],
+                "contact_name" => $row['contact_name'],
+                "phone_number" => $row['phone_number'],
+                "update" => "<button type='button' name='update' id='" . $row['id'] . "' class='btn btn-info btn-xs update' data-toggle='tooltip' title='Update'>Update</button>",
+                "delete" => "<button type='button' name='delete' id='" . $row['id'] . "' class='btn btn-danger btn-xs delete' data-toggle='tooltip' title='Delete'>Delete</button>",
+                "remark" => $row['remark']
+            );
+        } else {
+            $data[] = array(
+                "id" => $row['id'],
+                "house_number" => $row['house_number'],
+                "contact_name" => $row['contact_name'],
+                "select" => "<button type='button' name='select' id='" . $row['house_number'] . "@" . $row['contact_name'] . "' class='btn btn-outline-success btn-xs select' data-toggle='tooltip' title='select'>select <i class='fa fa-check' aria-hidden='true'></i>
+</button>",
+            );
+        }
+
+    }
+
+## Response Return Value
+    $response = array(
+        "draw" => intval($draw),
+        "iTotalRecords" => $totalRecords,
+        "iTotalDisplayRecords" => $totalRecordwithFilter,
+        "aaData" => $data
+    );
+
+    echo json_encode($response);
+
+}
