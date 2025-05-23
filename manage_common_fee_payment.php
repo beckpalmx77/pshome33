be744e90731744a80e5a10d4aa73b7055ecc1f1b
beckpalmx77 <beckpalmx@gmail.com>
2025-05-23 22:19:01 +0700

    Update manage_common_fee_payment.php

--- manage_common_fee_payment.php
+++ manage_common_fee_payment.php
@@ -88,7 +88,7 @@ if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['house_number']) == ""
                                                     <th>Slip</th>
                                                     <th>สถานะ</th>
                                                     <th>Action</th>
-                                                    <th>Action</th>
+                                                    <th>ใบเสร็จ</th>
                                                 </tr>
                                                 </tfoot>
                                             </table>
