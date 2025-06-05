0f68a2373b9afe53955c826c14c0e85fcf86ca0e
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-06-05 14:19:27 +0700

    up

--- model/save_user_profile.php
+++ model/save_user_profile.php
@@ -5,6 +5,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $userId = $_POST['userId'] ?? '';
     $displayName = $_POST['displayName'] ?? '';
     $pictureUrl = $_POST['pictureUrl'] ?? '';
+    $f_name = $_POST['f_name'] ?? '';
+    $l_name = $_POST['l_name'] ?? '';
+    $line_phone = $_POST['line_phone'] ?? '';
+    $detail = $f_name . " " . $l_name;
+
+    $myfile = fopen("a-param.txt", "w") or die("Unable to open file!");
+    fwrite($myfile, $pictureUrl  . " | " . $f_name . " | " . $l_name
+    . " | " . $line_phone . " | " . $detail . " | " . $displayName . " | " . $userId );
+    fclose($myfile);
 
     if (!empty($userId)) {
         // UPDATE หรือ INSERT ถ้าไม่มี record
@@ -14,7 +23,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $exists = $stmt->fetchColumn();
 
         if ($exists) {
-            $sql = "UPDATE ims_house_line_user SET line_user_name = :displayName, line_picture_profile = :pictureUrl WHERE line_user_id = :userId";
+            $sql = "UPDATE ims_house_line_user SET line_user_name = :displayName, line_picture_profile = :pictureUrl
+                    ,f_name = :f_name ,l_name = :l_name     
+                    WHERE line_user_id = :userId";
         }
 
         $stmt = $conn->prepare($sql);
@@ -24,17 +35,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $stmt->execute();
 
         if ($exists) {
-            $sql = "UPDATE ims_house_payment SET line_picture_profile_show = :pictureUrl WHERE line_user_id = :userId";
+            $sql = "UPDATE ims_user SET first_name = :f_name ,last_name = :l_name     
+                    WHERE user_id = :line_phone";
         }
 
         $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':f_name', $f_name);
+        $stmt->bindParam(':l_name', $l_name);
+        $stmt->bindParam(':line_phone', $line_phone);
+        $stmt->execute();
+
+        if ($exists) {
+            $sql = "UPDATE ims_house_payment SET line_picture_profile_show = :pictureUrl , detail = :detail WHERE line_user_id = :userId";
+        }
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':detail', $detail);
         $stmt->bindParam(':pictureUrl', $pictureUrl);
         $stmt->bindParam(':userId', $userId);
         $stmt->execute();
 
         echo 'success';
+
     } else {
         echo 'invalid';
     }
 }
-?>
+
