9f913b27ddcca39a67f6502a732705b64b15a90c
beckpalmx77 <beckpalmx@gmail.com>
2025-06-15 08:06:06 +0700

    Update get_requester.php

--- model/get_requester.php
+++ model/get_requester.php
@@ -4,15 +4,19 @@ include('../config/connect_db.php');
 if (isset($_POST['query'])) {
     $query = '%' . $_POST['query'] . '%';
 
-    // เปลี่ยนเป็น table และ field จริงที่คุณใช้
-    $stmt = $conn->prepare("SELECT requester_id, requester_name 
-    FROM ims_requester WHERE requester_name LIKE ? ORDER BY requester_name LIMIT 10");
+    $stmt = $conn->prepare("
+        SELECT emp_id, CONCAT(f_name, ' ', l_name) AS requester_name 
+        FROM memployee 
+        WHERE CONCAT(f_name, ' ', l_name) LIKE ? 
+        ORDER BY requester_name 
+        LIMIT 10
+    ");
     $stmt->execute([$query]);
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     foreach ($rows as $row) {
         echo '<a href="#" class="list-group-item list-group-item-action requester-item"
-                data-id="' . htmlspecialchars($row['requester_id']) . '"
+                data-id="' . htmlspecialchars($row['emp_id']) . '"
                 data-name="' . htmlspecialchars($row['requester_name']) . '">' .
             htmlspecialchars($row['requester_name']) .
             '</a>';
