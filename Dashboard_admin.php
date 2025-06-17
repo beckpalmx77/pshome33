b763dc5aca8111cee59287506193a1c83c422827
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-06-17 12:39:10 +0700

    Update Dashboard_admin.php

--- Dashboard_admin.php
+++ Dashboard_admin.php
@@ -200,7 +200,8 @@ if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['department_id']) == "
                                                         จำนวนบ้านที่ลงทะเบียน ระบบส่วนกลางพฤกษา33 ที่ขึ้นต้นด้วย 67
                                                     </div>
                                                     <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                        <?php echo number_format($count_67); ?> หลัง จาก  <?php echo number_format($count_67_house_master); ?> หลัง
+                                                        <?php echo number_format($count_67); ?> หลัง
+                                                        จาก <?php echo number_format($count_67_house_master); ?> หลัง
                                                         <?php $percent_master = ($count_67 / $count_67_house_master) * 100; ?>
                                                         <?php echo "คิดเป็น <strong>" . number_format($percent_master, 2) . "%</strong> ของบ้านทั้งหมด"; ?>
                                                     </div>
@@ -221,7 +222,8 @@ if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['department_id']) == "
                                                         จำนวนบ้านที่ลงทะเบียน ระบบส่วนกลางพฤกษา33 ที่ขึ้นต้นด้วย 68
                                                     </div>
                                                     <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                        <?php echo number_format($count_68); ?> หลัง จาก  <?php echo number_format($count_68_house_master); ?> หลัง
+                                                        <?php echo number_format($count_68); ?> หลัง
+                                                        จาก <?php echo number_format($count_68_house_master); ?> หลัง
                                                         <?php $percent_master = ($count_68 / $count_68_house_master) * 100; ?>
                                                         <?php echo "คิดเป็น <strong>" . number_format($percent_master, 2) . "%</strong> ของบ้านทั้งหมด"; ?>
                                                     </div>
