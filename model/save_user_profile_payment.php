cc9aeccc5bf56844b66ce83c5b1670ff07873919
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-07-12 14:41:35 +0700

    up

--- model/save_user_profile_payment.php
+++ model/save_user_profile_payment.php
@@ -8,6 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $line_phone = $_POST['line_phone'] ?? '';
     $line_email = "-";
 
+/*
+    $myfile = fopen("a-profile.txt", "w") or die("Unable to open file!");
+    fwrite($myfile, " Row Record = " . $userId . " Display Name = " . $displayName . " pictureUrl = " . $pictureUrl . " line_phone = " . $line_phone);
+    fclose($myfile);
+*/
+
     if (!empty($userId)) {
         // UPDATE หรือ INSERT ถ้าไม่มี record
         $stmt = $conn->prepare("SELECT COUNT(*) FROM ims_house_line_user WHERE line_user_id = :userId");
