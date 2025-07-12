073af754310efe52ebea6312f43450263f8d036e
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-07-12 11:07:59 +0700

    Update Footer.php

--- includes/Footer.php
+++ includes/Footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+            <span> <img src="img/logo/niti_ps33_header200.png" alt="" width="100" height="42.5"> copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
               <b><a href="#" target="_blank">System Admin</a></b>
             </span>
         </div>
