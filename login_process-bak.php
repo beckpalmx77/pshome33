92ccb5b60f83c0c9c94aab0b8d7fbd9c40bcb916
beckpalmx77 <beckpalmx@gmail.com>
2025-09-03 13:36:15 +0700

    up

--- login_process-bak.php
+++ login_process-bak.php
@@ -1,23 +1,27 @@
 <?php
 session_start();
-// ตั้งค่าให้แสดง error ทั้งหมดสำหรับการ debug
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
+error_reporting(0);
 include('config/connect_db.php');
 include('config/lang.php');
 include('util/GetData.php');
 include('includes/CheckDevice.php');
 
-// ล้างค่า session ที่เกี่ยวข้องกับการล็อกอินเดิม
-if (isset($_SESSION['alogin'])) {
+
+if ($_SESSION['alogin'] != '') {
     $_SESSION['alogin'] = '';
 }
 
 $username = $_POST['username'];
-$password = $_POST['password']; // รับค่ารหัสผ่านมาโดยตรงเพื่อใช้ตรวจสอบ
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$remember = $_POST['remember'];
 
-$sql = "SELECT iu.*, ih.house_number, ih.contact_name, pm.dashboard_page, ihu.line_picture_profile, ihu.line_phone
+/*
+$myfile = fopen("login-a.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $username . " | " . $password);
+fclose($myfile);
+*/
+
+$sql = "SELECT iu.*, ih.house_number, ih.contact_name, pm.dashboard_page, ihu.line_picture_profile,ihu.line_phone
         FROM ims_user iu
         LEFT JOIN ims_permission pm ON pm.permission_id = iu.account_type
         LEFT JOIN ims_house ih ON ih.phone_number = iu.user_id
@@ -31,10 +35,7 @@ $results = $query->fetchAll(PDO::FETCH_OBJ);
 
 if ($query->rowCount() == 1) {
     foreach ($results as $result) {
-        // ใช้ password_verify() เพื่อตรวจสอบรหัสผ่านที่ป้อนกับรหัสผ่านที่ถูก hash ในฐานข้อมูล
-        if (password_verify($password, $result->password)) {
-
-            // ตั้งค่า Session เมื่อล็อกอินสำเร็จ
+        if (password_verify($_POST['password'], $result->password)) {
             $_SESSION['alogin'] = $result->user_id;
             $_SESSION['login_id'] = $result->id;
             $_SESSION['username'] = $result->email;
@@ -56,30 +57,33 @@ if ($query->rowCount() == 1) {
             $_SESSION['phone_number'] = $result->line_phone;
             $_SESSION['user_signature'] = $result->user_signature;
 
-            // ตั้งค่า Cookie ที่ปลอดภัยยิ่งขึ้น (ไม่เก็บรหัสผ่าน)
-            if ($remember == "on") {
-                // เก็บแค่ username เพื่อใช้กรอกอัตโนมัติเท่านั้น
-                // ไม่จำเป็นต้องเก็บค่า 'remember_chk' เพราะสามารถตรวจสอบจาก cookie username ได้
-                setcookie("username", $username, time() + (86400 * 30), "/"); // Cookie มีอายุ 30 วัน
+/*
+            $myfile = fopen("login.txt", "w") or die("Unable to open file!");
+            fwrite($myfile, $_SESSION['house_number']);
+            fclose($myfile);
+*/
+
+            if ($remember == "on") { // ถ้าติ๊กถูก Login ตลอดไป ให้ทำการสร้าง cookie
+                setcookie("username", $_POST["username"], time() + (86400 * 10000), "/");
+                setcookie("password", $_POST["password"], time() + (86400 * 10000), "/");
+                setcookie("remember_chk", "check", time() + (86400 * 10000), "/");
             } else {
-                // ถ้าไม่ได้เลือก remember me ให้ลบ cookie ทิ้ง
-                setcookie("username", "", time() - 3600, "/");
+                setcookie("username", $_POST["username"], time() + (86400 * 10000), "/");
+                setcookie("password", $_POST["password"], time() + (86400 * 10000), "/");
+                setcookie("remember_chk", "check", time() + (86400 * 10000), "/");
             }
 
-            // ส่งค่า dashboard page ที่ถูกต้องกลับไป
-            if ($_SESSION['deviceType'] === 'computer' || $_SESSION['deviceType'] === 'tablet') {
+            //echo $result->dashboard_page;
+
+            if ($_SESSION['deviceType']==='computer' || $_SESSION['deviceType']==='tablet') {
                 echo $result->dashboard_page;
             } else {
+                //echo "payment_transfer_smart";
                 echo $result->dashboard_page;
             }
 
         } else {
-            // รหัสผ่านไม่ถูกต้อง
             echo 0;
         }
     }
-} else {
-    // ไม่พบผู้ใช้
-    echo 0;
 }
-?>
