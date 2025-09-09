86c35c9ca2541e44b901b0f44a668e798130be31
beckpalmx77 <beckpalmx@gmail.com>
2025-09-09 11:57:30 +0700

    Update Footer.php

--- includes/Footer.php
+++ includes/Footer.php
@@ -1,4 +1,15 @@
 <!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span><img src="img/logo/niti_ps33_header200.png" alt="" width="100" height="42.5"></span>
+            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> developed by <b><a href="#" target="_blank">System Admin</a></b>
+        </div>
+    </div>
+</footer>
+<!-- Footer -->
+
+<!-- Footer -->
 <!--div class="row">
     <div class="col-12">
         <div class="card">
@@ -15,17 +26,4 @@
         </div>
     </div>
 </div-->
-<!-- Footer -->
-
-
-
-<!-- Footer -->
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span><img src="img/logo/niti_ps33_header200.png" alt="" width="100" height="42.5"></span>
-            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> developed by <b><a href="#" target="_blank">System Admin</a></b>
-        </div>
-    </div>
-</footer>
 <!-- Footer -->
