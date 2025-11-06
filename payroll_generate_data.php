67d05a3943d75ffafe1f2173e8936a49dacc7fd6
beckpalmx77 <beckpalmx@gmail.com>
2025-11-06 11:50:19 +0700

    Update payroll_generate_data.php

--- payroll_generate_data.php
+++ payroll_generate_data.php
@@ -82,12 +82,13 @@ if (strlen($_SESSION['alogin']) == "") {
 
                 <div class="container-fluid" id="container-wrapper">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800"><?php echo $sub_menu_name; ?></h1>
+                        <h1 class="h4 mb-0 text-gray-800"><?php echo urldecode($_GET['s']) ?></h1>
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="<?php echo $dashboard_page; ?>">Home</a></li>
-                            <li class="breadcrumb-item"><?php echo $main_menu_name; ?></li>
+                            <li class="breadcrumb-item"><a href="<?php echo $_SESSION['dashboard_page'] ?>">Home</a>
+                            </li>
+                            <li class="breadcrumb-item"><?php echo urldecode($_GET['m']) ?></li>
                             <li class="breadcrumb-item active"
-                                aria-current="page"><?php echo $sub_menu_name; ?></li>
+                                aria-current="page"><?php echo urldecode($_GET['s']) ?></li>
                         </ol>
                     </div>
 
