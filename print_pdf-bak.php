ec21c261fef6bf4f008c316b2d8498af0ce04a02
beckpalmx77 <beckpalmx@gmail.com>
2026-02-08 17:28:17 +0700

    up

--- print_pdf-bak.php
+++ print_pdf-bak.php
@@ -6,7 +6,6 @@ include('util/number_to_thai_text.php');
 
 // ตรวจสอบค่า ID
 $id = isset($_GET['id']) ? $_GET['id'] : '';
-if (!$id) {
     die("ไม่พบข้อมูล");
 }
 
@@ -42,7 +41,7 @@ class CustomPDF extends TCPDF
 
     public function Footer()
     {
-        // ไม่ต้องทำ footer ของ TCPDF เพราะเรากำหนด Footer ใน HTML แล้ว
+        // ไม่ต้องทำ footer เพราะกำหนดใน HTML แล้ว
     }
 }
 
@@ -60,9 +59,7 @@ $pdf->SetFont('THSarabunNew', '', 12);
 
 $pdf->AddPage();
 
-// -----------------------------------------------------------------------------
 // ฟังก์ชันสร้าง HTML สำหรับใบเสร็จ
-// -----------------------------------------------------------------------------
 function generate_receipt_html($company, $receipt, $items, $total, $thai_text_total, $title_note = '')
 {
     $full_name = $_SESSION['first_name'] . " " . $_SESSION['last_name'];
@@ -72,35 +69,27 @@ function generate_receipt_html($company, $receipt, $items, $total, $thai_text_to
         ? '<img src="' . $signature_path . '" height="30">'
         : '____________';
 
-    // --- ส่วนหัวกระดาษ ---
     $html = '
-    <table width="100%" cellspacing="0" cellpadding="0" style="margin-bottom:10px; margin-top:10px;">
+    <table width="100%" cellspacing="0" cellpadding="0" style="margin-bottom:20px; margin-top:20px; text-align:center;">
         <tr>
-            <td width="70%" align="left" valign="top">
-                <b style="font-size:14pt;">' . $company['company_name'] . '</b><br>
-                <span style="font-size:12pt;">' . $company['address_1'] . ' ' . $company['address_2'] . ' ' . $company['state'] . ' ' . $company['zip_code'] . '</span><br>
-                <img src="img/logo/niti_ps33_header.png" height="40" style="display: block; margin-top: 5px;">
-            </td>
-            
-            <td width="30%" align="right" valign="top">
-                <h2 style="margin-top: 0;">ใบเสร็จรับเงิน ' . $title_note . '</h2>
+            <td>
+                <h2 style="margin-bottom: 5px;">ใบเสร็จรับเงิน ' . $title_note . '</h2>
+                <img src="img/logo/niti_ps33_header.png" height="40" style="display: block; margin: 0 auto;">
             </td>
         </tr>
     </table>
 
     <table border="0" cellspacing="0" cellpadding="4" width="100%" style="font-size:12pt;">
         <tr>        
-            <td width="60%" valign="top">
-                <b>บ้านเลขที่ : ' . $receipt['house_number'] . ' ซอย : ' . $receipt['alley'] . ' &nbsp; ผู้ชำระเงิน : ' . $receipt['detail'] . '</b>
-            </td>            
-            <td width="40%" align="right" valign="top">
-                <b>เลขที่ใบเสร็จ: ' . $receipt['doc_id'] . '</b><br>
-                <b>วันที่: ' . date('d/m/Y', strtotime($receipt['payment_date'])) . '</b>
-            </td>
+            <td><b>' . $company['company_name'] . '</b></td>
+            <td align="right"><b>เลขที่ใบเสร็จ:</b> ' . $receipt['doc_id'] . '</td>
+        </tr>
+        <tr>
+            <td><b>ที่อยู่:</b> ' . $company['address_1'] . ' ' . $company['address_2'] . ' ' . $company['state'] . ' ' . $company['zip_code'] . '</td>
+            <td align="right"><b>วันที่:</b> ' . date('d/m/Y', strtotime($receipt['payment_date'])) . '</td>
         </tr>
     </table>';
 
-    // --- ส่วนตารางรายการ ---
     $html .= '<table border="1" cellspacing="0" cellpadding="5" width="100%" style="table-layout: fixed; font-size:12pt;">
         <tr style="background-color:#f2f2f2;">
             <th width="10%" align="center"><b>#</b></th>
@@ -116,10 +105,10 @@ function generate_receipt_html($company, $receipt, $items, $total, $thai_text_to
 
         $html .= '<tr>
             <td align="center">' . ($index + 1) . '</td>
-            <td><b>ค่าส่วนกลาง งวดเดือน </b><b> ' . $period_month . ' ' . $receipt['period_year']+543 . '</b> </td>
+            <td><b>ค่าส่วนกลาง บ้านเลขที่ ' . $receipt['house_number'] . ' ซอย ' . $receipt['alley'] . ' งวดเดือน </b> ' . $period_month . ' ' . $receipt['period_year'] . ' </td>
 
-            <td align="right"><b>1</b></td>
-            <td align="right"><b>' . number_format($item['amount'], 2) . '</b></td>
+            <td align="right">1</td>
+            <td align="right">' . number_format($item['amount'], 2) . '</td>
         </tr>';
     }
 
@@ -130,15 +119,14 @@ function generate_receipt_html($company, $receipt, $items, $total, $thai_text_to
 </tr>';
 
     $html .= '<tr>
-        <td colspan="4" align="right"><i><b>( ' . $thai_text_total . ' )</b></i></td>
+        <td colspan="4" align="right"><i>( ' . $thai_text_total . ' )</i></td>
     </tr>';
 
     $html .= '</table><br><br>';
 
     $html .= '<table border="0" cellspacing="0" cellpadding="5" width="100%" style="margin-top:20px; margin-bottom:20px; font-size:12pt;">
 <tr>
-    <!--td align="left"><b>ผู้ชำระเงิน</b> _______________ (' . $receipt['detail'] . ')</td-->
-    <td align="left"></td>
+    <td align="left"><b>ผู้ชำระเงิน</b> ___________ (' . $receipt['detail'] . ')</td>
     <td align="center">
         <b>ผู้รับเงิน</b><br>
         ' . $signature_img . '<br>
@@ -158,23 +146,10 @@ function generate_receipt_html($company, $receipt, $items, $total, $thai_text_to
     return $html;
 }
 
-// -----------------------------------------------------------------------------
-// เริ่มการประกอบร่าง HTML
-// -----------------------------------------------------------------------------
-
-// 1. ส่วนต้นฉบับ
+// รวม HTML สองชุด (ต้นฉบับ + สำเนา) โดยเว้น space ระหว่างต้นฉบับกับสำเนา
 $html = generate_receipt_html($company, $receipt, $items, $total, $thai_text_total, "(ต้นฉบับ)");
-
-// 2. ตัวแบ่งและเว้นวรรค
-$html .= '<br>'; // เว้นบรรทัดก่อนเส้น
-$html .= '<hr style="border-top: dashed 1px; margin: 10px 0;">'; // เส้นประ
-
-// *** จุดแก้ไข: เพิ่ม div สูง 30px เพื่อดันหัวข้อสำเนาลงมาให้ห่างจากเส้น ***
-$html .= '<div style="line-height: 30px;">&nbsp;</div>';
-
-// 3. ส่วนสำเนา
+$html .= '<hr style="border-top: dashed 1px; margin: 30px 0;">';  // space เพิ่มขึ้นระหว่างต้นฉบับกับสำเนา
 $html .= generate_receipt_html($company, $receipt, $items, $total, $thai_text_total, "(สำเนา)");
-
 
 // เขียนลง PDF
 $pdf->writeHTML($html, true, false, false, false, '');
@@ -196,4 +171,3 @@ $stmt_items->execute();
 // สร้างชื่อไฟล์
 $filename = 'receipt_' . $receipt['doc_id'] . '_' . date('Ymd_His') . '.pdf';
 $pdf->Output($filename, 'I');
-?>
