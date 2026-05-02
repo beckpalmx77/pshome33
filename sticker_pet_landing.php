e4fc815b885725137a5e62cd1939e9a84d12954b
beckpalmx77 <beckpalmx@gmail.com>
2026-05-02 14:09:34 +0700

    Update sticker_pet_landing.php

--- sticker_pet_landing.php
+++ sticker_pet_landing.php
@@ -22,7 +22,7 @@ include('includes/Header.php');
             <!-- Container Fluid-->
             <div class="container-fluid" id="container-wrapper">
                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 class="h5 mb-0 text-gray-800">LANDING PAGE</h1>
+                    <h1 class="h5 mb-0 text-gray-800">หมู่บ้านพฤกษา 33</h1>
                     <br>
                     <!-- โปรไฟล์และข้อมูลผู้ใช้ -->
                     <div class="d-flex align-items-center gap-3">
@@ -39,7 +39,7 @@ include('includes/Header.php');
 
                 <!-- สถิติรถและสติกเกอร์ -->
                 <div class="row mb-4">
-                    <div class="col-xl-4 col-md-6 mb-4">
+                    <!--div class="col-xl-4 col-md-6 mb-4">
                         <div class="card h-100">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
@@ -55,7 +55,9 @@ include('includes/Header.php');
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div-->
+
+                    <input type="hidden" id="totalCars">
 
                     <div class="col-xl-4 col-md-6 mb-4">
                         <div class="card h-100">
