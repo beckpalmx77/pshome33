564b6028a69c01d4fdef9bcb7a94121d18336d06
beckpalmx77 <beckpalmx@gmail.com>
2026-05-27 12:45:55 +0700

    Update manage-line-webhook-messages.php

--- manage-line-webhook-messages.php
+++ manage-line-webhook-messages.php
@@ -75,7 +75,8 @@ if (strlen($_SESSION['alogin']) == "" || strlen($_SESSION['department_id']) == "
     <script>
         $(document).ready(function () {
             let dataRecords = $('#TableRecordList').DataTable({
-                'lengthMenu': [[10, 20, 50, 100], [10, 20, 50, 100]],
+                'lengthMenu': [[5, 10, 20, 50, 100], [5, 10, 20, 50, 100]],
+                'pageLength': 5,
                 'processing': true,
                 'serverSide': true,
                 'serverMethod': 'post',
