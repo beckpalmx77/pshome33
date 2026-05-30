f9f93b9a9462a6b084c53dee2a2f6054f321fd91
beckpalmx77 <beckpalmx@gmail.com>
2026-05-30 11:56:53 +0700

    up

--- report_pnl.php
+++ report_pnl.php
@@ -22,11 +22,19 @@ if (strlen($_SESSION['alogin']) == "") {
         <?php include('includes/Side-Bar.php'); ?>
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
-                <?php include('includes/Top-Bar.php'); ?>
-
+                <?php
+                include('includes/Top-Bar.php');
+                ?>
                 <div class="container-fluid" id="container-wrapper">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h4 mb-0 text-gray-800">รายงานงบกำไรขาดทุน (Profit & Loss)</h1>
+                        <h1 class="h3 mb-0 text-gray-800"><?php echo urldecode($_GET['s']) ?></h1>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?php echo $_SESSION['dashboard_page'] ?>">Home</a>
+                            </li>
+                            <li class="breadcrumb-item"><?php echo urldecode($_GET['m']) ?></li>
+                            <li class="breadcrumb-item active"
+                                aria-current="page"><?php echo urldecode($_GET['s']) ?></li>
+                        </ol>
                     </div>
 
                     <div class="row">
