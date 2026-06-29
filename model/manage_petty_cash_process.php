f75dd6c206ee2e2f861cd52cd555f40cd3f12b3e
beckpalmx77 <beckpalmx@gmail.com>
2026-06-29 17:29:58 +0700

    Update manage_petty_cash_process.php

--- model/manage_petty_cash_process.php
+++ model/manage_petty_cash_process.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
-//error_reporting(0);
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(0);
 
 include('../config/connect_db.php');
 include('../config/lang.php');
